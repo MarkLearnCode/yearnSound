b71cbebc50c824b9f0da0ac113b31c046a8aa0fd
MarkLearnCode <ppopfg@gmail.com>
2024-08-05 16:08:47 +0800

    config setting
    config setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@kevinmarrec/nuxt-pwa'
+  ],
+  pwa: {
+    manifest: {
+      name: 'Your App Name',
+      short_name: 'App',
+      lang: 'en',
+      start_url: '/',
+      display: 'standalone'
+    }
+  }
 })
